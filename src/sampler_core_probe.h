f84a26c867477967fec581edbd6ab4315632ad67
mpereiraesaa <manuelpereiralds@gmail.com>
2026-09-24 23:03:49 +0200

    Define U and V mirror-clamp CPU reference cases (#457)

--- src/sampler_core_probe.h
+++ src/sampler_core_probe.h
@@ -2,7 +2,8 @@
 #define PS5VK_SAMPLER_CORE_PROBE_H
 #include <stdint.h>
 #include <vulkan/vulkan_core.h>
-enum { PS5VK_SAMPLER_CORE_CASES = 8 };
+#define PS5VK_SAMPLER_MIRROR_FIRST_CASE 8
+enum { PS5VK_SAMPLER_CORE_CASES = 20 };
 struct ps5vk_sampler_core_case {
     const char *name;
     VkSamplerAddressMode address_mode;
@@ -13,6 +14,10 @@ struct ps5vk_sampler_core_case {
     uint32_t expected_bgra;
     unsigned checkerboard;
     unsigned minification;
+    /* Zero keeps the older both-axis cases; 1 and 2 select U and V. */
+    float uv_v;
+    unsigned mirror_axis;
 };
 int ps5vk_sampler_core_case(unsigned, struct ps5vk_sampler_core_case *);
+int ps5vk_sampler_core_color_near(uint32_t actual,uint32_t expected,unsigned tolerance);
 #endif
